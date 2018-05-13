d231c70bbaf3e06924a1a352349c77e91313c0fa
nhcminh <a2mrken@gmail.com>
2018-05-13 21:34:29 +0700

    Update UserController.java

--- project/src/java/Controller/UserController.java
+++ project/src/java/Controller/UserController.java
@@ -19,6 +19,7 @@ public class UserController {
     private static DatabaseConnection db;
     private static Statement stm;
     private static ResultSet re;
+    private static User user;
 
     static void connection(String query) throws ClassNotFoundException, SQLException {
         db = new DatabaseConnection();
@@ -27,12 +28,30 @@ public class UserController {
     }
 
     public boolean login(String username, String password) throws ClassNotFoundException, SQLException {
-        connection("select Username, Password from user");
+        connection("select * from user");
         while (re.next()) {
-            return re.getString("Username").equals(username) && re.getString("Password").equals(password);
+            if (username.equals(re.getString("Username")) && password.equals(re.getString("Password"))) {
+                return true;
+            }
         }
-        
         return false;
+    }
+    public void Register(User user) throws SQLException{
+        stm.executeUpdate("insert into user values ('"+user.getUsername()+"','"+user.getFullname()+"','"+user.getEmail()+"','"+user.getPassword()+"','Author',)");
+    }
+    public User getInfor(String username) throws ClassNotFoundException, SQLException {
+        connection("select Username, Fullname, Email, Role from user where Username='"+username+"'");
+        user = new User();
+        re.next();
+        String Fullname = re.getString("Fullname");
+        String Username = re.getString("Username");
+        String Email = re.getString("Email");
+        String Role = re.getString("Role");
+        user.setFullname(Fullname);
+        user.setUsername(Username);
+        user.setEmail(Email);
+        user.setRole(Role);
+        return user;
     }
 
     public UserController() {
@@ -41,17 +60,25 @@ public class UserController {
     public boolean register(User user, String pass2) throws ClassNotFoundException, SQLException {
         connection("select * from User");
         while (re.next()) {
-            return !(user.getFullname().equals(re.getString("Fullname")) || user.getEmail().equals("Email") || user.getUsername().equals("Username") || user.getPassword()!=pass2);
+            return !(user.getFullname().equals(re.getString("Fullname")) || user.getEmail().equals("Email") || user.getUsername().equals("Username") || user.getPassword() != pass2);
         }
         return false;
     }
-    public void resetpassword(){}
-    public void changpassword(String username, String newpassword) throws ClassNotFoundException, SQLException{
-        connection("update User set password ='"+newpassword+"' where username='"+username+"'");
+
+    public void resetpassword() {
     }
-    public void search(){}
-    public void editprofile(String username, User user) throws ClassNotFoundException, SQLException{
-        connection("update User set password ='"+user.getFullname()+"' where username='"+username+"'");
-        connection("update User set password ='"+user.getEmail()+"' where username='"+username+"'");
+
+    public void changpassword(String username, String newpassword) throws ClassNotFoundException, SQLException {
+        connection("update User set Password ='" + newpassword + "' where Username='" + username + "'");
+    }
+
+    public void search() {
+    }
+
+    public void editprofile(String username, User user) throws ClassNotFoundException, SQLException {
+        System.out.println(username);
+        System.out.println(user.getFullname());
+        stm.executeUpdate("update User set Fullname ='" + user.getFullname() + "' where Username='" + username + "'");
+        stm.executeUpdate("update User set Email ='" + user.getEmail() + "' where Username='" + username + "'");
     }
 }
